8f4187728f02fa1f31093e7dec8b11f325bf1c81
mlefter <m.lefter@lumc.nl>
2017-09-19 07:18:30 +0200

    Added solutions.

---
+++ solutions/ex4_combining_lists.py
@@ -0,0 +1,7 @@
+# Exercise 4: Combining lists
+# ==========
+#
+# Calculate all coordinates of the line x=y with x < 100.
+# - Note: This is the sequence (0, 0), (1, 1), ... (99, 99)
+
+print zip(range(100), range(100))
